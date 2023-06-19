e7e109809b386ddafa35c43ac188b78a6d234b63
Chris K. Thomas <135461249+chriskthomas@users.noreply.github.com>
2023-06-19 17:06:15 +0000

    enable possibility of js theme files

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This is a collection of themes for [LinkFree Generator](https://github.com/chris
 - **Cherry-Bloosm:** No animation.
 - **NyanCat** No animation.
 - **PurpleSideOfTheForce** No audio.
+
+## Development
+
+Themes are contained in the `themes` directory. You can add your own themes by adding a new directory with a slug that represents the theme name and adding the following files:
+
+- `NAME`: The theme's name. This is the name that will be displayed in the generator.
+- `style.css`: The theme's css file. Technically optional, but you probably want one.
+- `script.js`: Optional theme javascript file.
+- `/images`: Optional directory for theme images. The theme's css file (and/or js file) should reference images in this directory.
+- `/audio`: Optional directory for theme audio files. The theme's js file should reference audio files in this directory.
--- make-index.js
+++ make-index.js
@@ -1,25 +1,35 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const index_obj = {};
 
 // Iterate through the themes folder and read the contents of each `NAME` file
 // to get the theme name. Then output a JSON array of objects with the theme
-fs.readdir('themes', (err, dirs) => {
+fs.readdir("themes", (err, dirs) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  for (const dir of dirs) {
+    // Add the theme name to the object
+    const theme_name = fs.readFileSync(`themes/${dir}/NAME`, "utf8");
+    index_obj[dir] = { id: dir, name: theme_name.trim() };
+
+    // Check if the css and js files exist and add them to the object if they do
+    const potential_css_path = `themes/${dir}/style.css`;
+    const potential_js_path = `themes/${dir}/script.js`;
+    if (fs.existsSync(potential_css_path)) {
+      index_obj[dir].css = potential_css_path;
+    }
+    if (fs.existsSync(potential_js_path)) {
+      index_obj[dir].js = potential_js_path;
+    }
+  }
+
+  // After the array is built, write it to the index.json file
+  fs.writeFile("index.json", JSON.stringify(index_obj), "utf8", (err) => {
     if (err) {
-        console.error(err);
-        return;
+      console.error(err);
+      return;
     }
-    for (const dir of dirs) {
-        const theme_name = fs.readFileSync(`themes/${dir}/NAME`, 'utf8')
-        index_obj[dir] = { id: dir, name: theme_name.trim(), css: `themes/${dir}/style.css` };
-    }
-
-    // After the array is built, write it to the index.json file
-    fs.writeFile('index.json', JSON.stringify(index_obj), 'utf8', (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    });
+  });
 });
-
